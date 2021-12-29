ebc869eb0666c272c1d45ef37994b08a67329beb
Zhuo Chen <chen@0x1c.dev>
2021-12-29 18:51:19 +0800

    webpack configs (#1)
    * webpack configs
    
    * remove unused deps & esbuild script

---
+++ webpack.common.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const { ProvidePlugin } = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+function d(relPath) {
+  return path.resolve(__dirname, relPath);
+}
+
+module.exports = {
+  d,
+  config: {
+    entry: d('./src/index.tsx'),
+    output: {
+      path: d('./dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+      alias: {
+        '@': d('./src'),
+        vendor: d('./vendor'),
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new ProvidePlugin({ React: 'react' }),
+      new CopyWebpackPlugin({
+        patterns: [{ from: 'public' }],
+      }),
+    ],
+  },
+};
